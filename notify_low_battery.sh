eac82cbd5602bb9403d82d33ee9770c80d21d60d
AnyielFernandes <anyf23.5@gmail.com>
2020-10-03 18:28:23 +0200

    Fixed some bugs

--- notify_low_battery.sh
+++ notify_low_battery.sh
@@ -4,7 +4,8 @@
 
 warning_at=20
 battery_level=$( acpi | egrep -o "[0-9]{2}%" | head -c 2 ) 
+battery_full=$( acpi | egrep -o "100%" | head -c 2 ) 
 
-if [ $battery_level -lt $warning_at ] && acpi | grep "Discharging" &>/dev/null; then 
+if [ $battery_level -lt $warning_at ] && [ -z battery_full ] && acpi | grep "Discharging" &>/dev/null; then 
 	notify-send -i /home/anyel/allpha/icons/low-battery.svg "low battery" -u critical
 fi
